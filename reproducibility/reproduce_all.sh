989fd97e6c2cdb3d3c9ac343f9562b848e8daf0b
Rushil Anirudh <anirudh1@ipa15.llnl.gov>
2022-07-20 15:06:27 -0700

    added scood script

--- reproducibility/reproduce_all.sh
+++ reproducibility/reproduce_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for scr in reproduce_table2.sh reproduce_table7.sh reproduce_table12; do
+for scr in reproduce_table2.sh reproduce_table7.sh reproduce_scood.sh reproduce_table12; do
   bash $scr
 done
